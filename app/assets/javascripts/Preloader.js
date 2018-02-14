6e98dc4cc2397e2c977e68521a67ddc93af8a9a7
Steven Phuong <s.phuong@live.com>
2018-02-14 12:02:11 +1100

    links issue

--- app/assets/javascripts/Preloader.js
+++ app/assets/javascripts/Preloader.js
@@ -10,23 +10,23 @@ GAImmersered.Preloader.prototype = {
 		this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'preloaderBar');
 		this.preloadBar.anchor.setTo(0.5);
 		this.load.setPreloadSprite(this.preloadBar);
-		this.load.image('playButton', '/assets/play.png');
-		this.load.image('flame', '/assets/flame.png');
-		this.load.image('sword', '/assets/sword.png');
-		this.load.image('levelParticle', '/assets/level-particle.png');
-		this.load.image('spellParticle', '/assets/spell-particle.png');
+		this.load.image('playButton', 'assets/play.png');
+		this.load.image('flame', 'assets/flame.png');
+		this.load.image('sword', 'assets/sword.png');
+		this.load.image('levelParticle', 'assets/level-particle.png');
+		this.load.image('spellParticle', 'assets/spell-particle.png');
 		this.load.image('spaceButton', 'assets/spacebarButton.png');
 
-		this.load.spritesheet('tiles', '/assets/tiles.png', 16, 16);
-		this.load.spritesheet('things', '/assets/things.png', 16, 16);
-		this.load.spritesheet('characters', '/assets/characters.png', 16, 16);
-		this.load.spritesheet('dead', '/assets/dead.png', 16, 16);
-		this.load.spritesheet('potions', '/assets/potions.png', 16, 16);
-		this.load.spritesheet('dragons', '/assets/dragons.png', 32, 32);
-		this.load.spritesheet('fireball', '/assets/fireball.png', 16, 16);
-		this.load.spritesheet('spell', '/assets/spell.png', 12, 12);
+		this.load.spritesheet('tiles', 'assets/tiles.png', 16, 16);
+		this.load.spritesheet('things', 'assets/things.png', 16, 16);
+		this.load.spritesheet('characters', 'assets/characters.png', 16, 16);
+		this.load.spritesheet('dead', 'assets/dead.png', 16, 16);
+		this.load.spritesheet('potions', 'assets/potions.png', 16, 16);
+		this.load.spritesheet('dragons', 'assets/dragons.png', 32, 32);
+		this.load.spritesheet('fireball', 'assets/fireball.png', 16, 16);
+		this.load.spritesheet('spell', 'assets/spell.png', 12, 12);
 
-		this.load.audio('openingMusic', '/opening.ogg');
+		this.load.audio('openingMusic', 'opening.ogg');
 
 	},
 	create: function () {
